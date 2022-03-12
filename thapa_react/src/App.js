c1aa8bb71878a8f1dba2d21612c9a49b0ca78c07
vhald <vaibhavgsh@gmail.com>
2022-03-12 19:50:02 +0530

    react web pages

--- thapa_react/src/App.js
+++ thapa_react/src/App.js
@@ -1,9 +1,44 @@
 import React from 'react'
 
+// //fuctional  component of react. < App />
+
+// METHOD 1 <React Script - JSX format> (more preffered and easy)
+// We dont need to use import React from 'react' in this (acc. to react v17.0)
+// const App = () => {
+//   // this return is imp, as we want to get something in return from this component. 
+//   // JSX Must be returned only inside it.
+//   return (
+//       <div>
+//     <h1>Welcome Onboard MF!</h1>
+//       </div>
+//   )
+// }
+
+// METHOD 2 <React Script - React DOM>
+// const App = () => {
+//   // no matter whatever version of React, now we have to use line 1, use to React.createElement
+//   return React.createElement("h1", {}, "Hello World");
+// }
+
+// {/* <div><h3>hello world</h3></div> */} can be written as below: 
+// const App = () => {
+//   return React.createElement(
+//     "div", {}, React.createElement('h3', {}, "hello World")
+//   )
+// }
+
+// we can return only one element using JSX i.e. One complete {div/section/article/React.Fragment} element, or another complete element
+
+
 const App = () => {
-  return (
-    <div>Welcome Onboard MF!</div>
+  return(
+    // but remember that we need to import React first, inorder to use Fragment
+    <React.Fragment>
+      <h1>Hello world</h1>
+      <p>this is it.</p>
+    </React.Fragment>
   )
 }
+
 
 export default App
--- thapa_react/src/index.js
+++ thapa_react/src/index.js
@@ -10,5 +10,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+  
 
